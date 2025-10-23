54c1bd6d811d6225b885e2ce3187624b7cc9ea65
llyina <miss.ellyina@gmail.com>
2025-10-23 18:44:37 +0800

    first commit

---
+++ backend/house-rental-backend/src/main/java/com/llyinatech/houserental/HouseRentalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.llyinatech.houserental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseRentalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseRentalBackendApplication.class, args);
+    }
+
+}
---
+++ backend/house-rental-backend/src/test/java/com/llyinatech/houserental/HouseRentalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llyinatech.houserental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseRentalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
